e7e1cbefe682e63074ef18b00eda5ab4591c9cb5
K3nFischer <34724125+K3nFischer@users.noreply.github.com>
2022-02-24 01:53:14 -0800

    Create Cube.js

---
+++ Assignment3/Cube.js
@@ -0,0 +1,36 @@
+function Cube(gl) {
+
+    var program = initShaders(gl, "Cube-vertex-shader", "Cube-fragment-shader");
+
+    var positions = [
+        // --> Insert your vertex positions here
+    ];
+    
+    var indices = [
+        // --> Insert you index values here
+    ];
+
+    positions.numComponents = 3;
+
+    positions.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, positions.buffer );
+    gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW );
+
+    indices.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indices.buffer );
+    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW );
+
+    positions.aPosition = gl.getAttribLocation( program, "aPosition" );
+    gl.enableVertexAttribArray( positions.aPosition );
+
+    this.render = function () {
+        gl.useProgram( program );
+
+        gl.bindBuffer( gl.ARRAY_BUFFER, positions.buffer );
+        gl.vertexAttribPointer( positions.aPosition, positions.numComponents,
+            gl.FLOAT, false, 0, 0 );
+ 
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indices.buffer );
+        gl.drawElements( gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0 );
+    }
+};
