345418b60c6150ef7a6bef8364a1bfd8a49c2876
K3nFischer <kendalljfischer@gmail.com>
2022-03-15 00:30:16 -0700

    Planet colors set, matrix stack operations added

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -1,4 +1,6 @@
 var gl;
+var year;
+var day;
 
 function init() {
     var canvas = document.getElementById("webgl-canvas");
@@ -12,20 +14,49 @@ function init() {
     Earth = new Sphere(24, 24);
     Moon = new Sphere(12, 12);
 
+    Sun.color = vec4(1, .7, 0, 1);
+    Earth.color = vec4(0, .2, 1, 1);
+    Moon.color = vec4(.2, .2, .2, 1);
+
+    year = day = 0;
+
+
+
     requestAnimationFrame(render);
 }
 
 function render() {
 
     // Update your motion variables here
+    year++;
+    day++;
 
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
     
     // Add your rendering sequence here
 
     ms = new MatrixStack();
-    var v = translate(0.0, 0.0, 0.0, -0.5*(near + far))
+    var V = translate(0.0, 0.0, 0.0, -0.5*(1 + 30))
     ms.load(V)
+
+    ms.push();
+    ms.scale(3);
+    Sun.draw();
+    ms.pop();
+
+    ms.push();
+    ms.rotate(year, axis);
+    ms.translate(10, 0, 0);
+    ms.push();
+    ms.rotate(day, axis);
+    ms.scale(2);
+    Earth.draw();
+    ms.pop();
+
+    ms.translate(4, 0, 0);
+    ms.scale(1);
+    Moon.draw();
+    ms.pop();
 
     requestAnimationFrame(render);
 }
