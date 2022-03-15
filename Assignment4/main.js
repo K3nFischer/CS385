4bf1c37433c802dc4b13c421871e9ab67264f88a
K3nFischer <kendalljfischer@gmail.com>
2022-03-14 23:10:29 -0700

    Planets added to init

--- Assignment4/main.js
+++ Assignment4/main.js
@@ -1,6 +1,3 @@
-
-"use strict";
-
 var gl;
 
 function init() {
@@ -11,6 +8,9 @@ function init() {
     gl.enable(gl.DEPTH_TEST);
    
     // Add your sphere creation and configuration code here
+    Sun = new Sphere(36, 36);
+    Earth = new Sphere(24, 24);
+    Moon = new Sphere(12, 12);
 
     requestAnimationFrame(render);
 }
@@ -22,6 +22,10 @@ function render() {
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
     
     // Add your rendering sequence here
+
+    ms = new MatrixStack();
+    var v = translate(0.0, 0.0, 0.0, -0.5*(near + far))
+    ms.load(V)
 
     requestAnimationFrame(render);
 }
